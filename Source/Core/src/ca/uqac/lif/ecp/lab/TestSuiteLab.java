0d7f0dd8228c9deff7fea55bf94964f916e1f7dd
Sylvain Hallé <shalle@acm.org>
2016-11-15 10:36:49 -0500

    Multidimensional table for results

--- Source/Core/src/ca/uqac/lif/ecp/lab/TestSuiteLab.java
+++ Source/Core/src/ca/uqac/lif/ecp/lab/TestSuiteLab.java
@@ -1,6 +1,6 @@
 /*
     Log trace triaging and etc.
-    Copyright (C) 2016 Sylvain Hall�
+    Copyright (C) 2016 Sylvain Hallé
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published
