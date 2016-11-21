0ce073dacafa88bacdb0ecada4bea9098c87e3ec
Sylvain Hallé <shalle@acm.org>
2016-11-21 11:38:18 -0500

    Dotall

--- Source/CoreTest/dotall.zsh
+++ Source/CoreTest/dotall.zsh
@@ -1,6 +1,7 @@
 #! /usr/bin/zsh
+dot -Tsvg graph.dot > graph.svg
 for file in *.dot;
 do
-  echo ${file:r}.png
-  dot -Tpng $file > ${file:r}.png
+  echo ${file:r}.svg
+  dot -Tsvg $file > ${file:r}.svg
 done
